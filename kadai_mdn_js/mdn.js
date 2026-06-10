281858afbe80ec4a62377c2f1250ef28738ae9d5
YuN <yuukakomada098@gmail.com>
2026-06-10 17:08:15 +0900

    JS課題4（再提出）

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,9 @@
-const dateString = ('2024年10月12日')
-console.log(dateString)
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth() + 1, 
+  date.getDate(),
+  date.getFullYear(),
+];
+
+// 日付を出力する
+console.log(`${year}年 ${month}月 ${day}日`);
